d6cf589f450f8a63597ff0c2a61c78dd749c7f06
felpezneg1995 <felipe1995pezarininegrao@outlook.com>
2025-03-05 22:02:35 +0000

    Adcionando comentario e informações previas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
 
 int main () {
-     
+    //  Coloquei o numero  2 ao final de cada variavel de mesmo nome para a entrada de dados da segunda carta
+    // turis = Pontos turisticos / popul = População
+    // cidad = Nome da cidade / estad = Nome do estado
+    // letra = Letra da carta / numero = Numero da carta
+    
     int turis, popul, turis2, popul2;
     float area, pib, area2, pib2;
     char cidad[50], estad[50], cidad2[50], estad2[50];  
     char letra, numero, letra2, numero2; 
+    
+        /*Digite os dados que são pedidos seguindo a ordem de jogada de cada jogador 
+     Leia a informação que esta sendo requisitada
+     Jogador 1 insere o dado de sua carta
+     Jogador 2 insere o dado de sua carta 
+     Ao final de todas as perguntas são exibidas ambas as cartas na interface
+    */
+     
+    /*    CARTAS PARA TESTE
+Numero 1:                          Numero 2:
+Letra: A                           Letra: B
+Código: A01                        Código: B02
+Nome do Estado: São_Paulo          Nome do Estado: Rio_de_Janeiro
+Nome da Cidade: São_Paulo          Nome da Cidade: Rio_de_Janeiro
+População: 12325000                População: 6748000
+Área: 1521.11 km²                  Área: 1200.25 km²
+PIB: 699.28 bilhões de reais       PIB: 300.50 bilhões de reais
+Número de Pontos Turísticos: 50    Número de Pontos Turísticos: 30 
+    */
     
 
     
