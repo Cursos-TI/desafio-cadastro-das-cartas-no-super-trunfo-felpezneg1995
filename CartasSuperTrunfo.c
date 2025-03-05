269b7c02b6c63ccdeb1e12c90c31df937008b838
felpezneg1995 <felipe1995pezarininegrao@outlook.com>
2025-03-05 22:05:55 +0000

    Adcionando caixa de saida de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -77,7 +77,11 @@ Número de Pontos Turísticos: 50    Número de Pontos Turísticos: 30
    scanf("%d", &turis);
    printf("Jogador 2\n");
    scanf("%d", &turis2);    
-
+   
+   //  Saida de dados (Carta do jogador 1 e do jogador 2 são exibidas)
+printf("CARTA JOGADOR 1\n Codigo da Carta: %c %c\n Estado:%s\n Cidade:%s\n Pontos turisticos:%d\n População: %d\n Area: %.2f km²\n PIB: %.2f bilhões de reais\n", numero, letra, estad, cidad, turis, popul, area, pib);
+printf("CARTA JOGADOR 2\n Codigo da Carta: %c %c\n Estado:%s\n Cidade:%s\n Pontos turisticos:%d\n População: %d\n Area: %.2f km²\n PIB: %.2f bilhões de reais\n", numero2, letra2, estad2, cidad2, turis2, popul2, area2, pib2);
+    return 0;
     
 }
 
