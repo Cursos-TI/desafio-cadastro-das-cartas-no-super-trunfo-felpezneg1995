b4f9513ddbc7b61a3720ad8f729df9c28d5f069e
felpezneg1995 <felipe1995pezarininegrao@outlook.com>
2025-03-08 14:46:38 +0000

    adcionando cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -26,14 +26,14 @@ int main () {
 Numero 1:                                Numero 2:
 Letra: A                                 Letra: B
 Código: A01                              Código: B02
-Nome do Estado: São_Paulo                Nome do Estado: Rio_de_Janeiro
-Nome da Cidade: São_Paulo                Nome da Cidade: Rio_de_Janeiro
+Nome do Estado: Bahia                    Nome do Estado: Ceara
+Nome da Cidade: Salvador                 Nome da Cidade: Fortaleza
 Número de Pontos Turísticos: 50          Número de Pontos Turísticos: 30 
-População: 12325000                      População: 6748000
-Área: 1521.11 km²                        Área: 1200.25 km²
-PIB: 699.28 bilhões de reais             PIB: 300.50 bilhões de reais
-Densidade Populacional: 8102.47 hab/km²  Densidade Populacional: 5622.24 hab/km²
-PIB per Capita: 56724.32 reais           PIB per Capita: 44532.91 reais
+População:    2 417 678                  População: 2 428 678 
+Área: 693.442 km²                        Área: 312.353 km²
+PIB:  62 954 487 490 bilhões de reais    PIB: 73 436 128,432 bilhões de reais
+Densidade Populacional: 3 486.5 hab/km²  Densidade Populacional: 7 775.4 hab/km²
+PIB per Capita: 21 706.06 reais           PIB per Capita: 27 164.45 reais
     */
    //Abaixo caixa de perguntas com as entradas de dados
    printf("Numero da Carta: \n");
